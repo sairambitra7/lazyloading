83f39d1a08546c3a56a920f5ddad52ac23a1f27c
sairambitra7 <sairambitra7@gmail.com>
2018-04-21 14:52:40 +0530

    app routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: 'app/auth/auth.module#AuthModule' },
+  { path: 'auth', loadChildren: 'app/auth/auth.module#AuthModule' },
   { path: 'project', loadChildren: 'app/project/project.module#ProjectModule' },
   {
     path: '',
