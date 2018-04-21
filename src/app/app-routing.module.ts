3ba233e1efa353e868fb3b8a102e2ee87caeea39
sairambitra7 <sairambitra7@gmail.com>
2018-04-21 12:46:16 +0530

    lazy loading changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: 'app/auth/auth.module#AuthModule' },
+  { path: 'project', loadChildren: 'app/project/project.module#ProjectModule' },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
